e3e67ca9eda3e717c42e8e5db217ad99ccfea3ac
MIRA2548 <numira2548@gmail.com>
2024-11-26 14:52:52 +0600

    Add files via upload

---
+++ 1901031.py
@@ -0,0 +1,22 @@
+import streamlit as st
+st.title("KNUP")
+st.header('Design of an Indegenous Museum')
+st.write('Site: Sylhet,Bangladesh')
+
+st.write('Numira Siddiqua')
+st.write('Level-3,Term-2')
+st.write('1901031')
+
+
+st.write('''Knup , a traditional utensil of the Khasia tribe from Meghalaya, India. is Woven from cocunut leaves and serves as a rain shield and tool for collecting betel leaves, Its simple yet functional design provides durability and waterproofing protecting users from heavy monson rains. While also symbolizing the tribes close connection to nature. lightweight and portable ,the Knup reflects the Khasias resourcefulness and adaptibility to their rain prone environment''' )
+st.header('Axonometric View & Structural Analysis')
+image_path_1 = r'C:\Users\USER\Downloads\axono.png'
+st.image(image_path_1)
+
+st.write()
+image_path_2 = r'C:\Users\USER\Downloads\Material.png'
+st.header('MATERIAL ANALYSIS')
+st.image(image_path_2)
+width,height = 800,800
+
+st.write('The Project has been acomplished using the local materials that interpret the culture and heritage of the community and  serves the basic purpose of the Museum')
